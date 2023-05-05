6d13a02683fcb8c9bbede21fdae6baae54d14351
ShwetaShirodkar <tasktask86@gmail.com>
2023-05-06 03:06:41 +0530

    Forntend

---
+++ Dashboard/restapi/StudentDashboard.js
@@ -0,0 +1,18 @@
+ // Make an HTTP GET request to an API endpoint
+axios.post('http://localhost:8080/courses/list')
+    .then(httpResponse => {
+          form.reset()
+          console.log(httpResponse)
+
+          return httpResponse.data
+          .catch(err => {
+            console.log(err)
+            const errDv = document.getElementById('errMsg')
+            errDv.style.display = 'block'
+            errDv.innerHTML = `<strong>${err.response.data.msg}</strong>`
+        })
+        // .finally(function () {
+        //     // always executed
+        //
+    });
+
