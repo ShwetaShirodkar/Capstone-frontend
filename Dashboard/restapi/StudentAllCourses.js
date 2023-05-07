a7fd070f0f87e998be7a9daeb1b28094b2500793
ShwetaShirodkar <tasktask86@gmail.com>
2023-05-08 01:08:30 +0530

    front end

--- Dashboard/restapi/StudentAllCourses.js
+++ Dashboard/restapi/StudentAllCourses.js
@@ -1,18 +1,48 @@
- // Make an HTTP GET request to an API endpoint
- axios.post('http://localhost:8080/courses/list')
- .then(httpResponse => {
-       form.reset()
-       console.log(httpResponse)
+function setUpTable() {
+  const table = document.getElementById('tableCourse')
+  apiFetchAllCourses(table)
+}
 
-       return httpResponse.data
-       .catch(err => {
-         console.log(err)
-         const errDv = document.getElementById('errMsg')
-         errDv.style.display = 'block'
-         errDv.innerHTML = `<strong>${err.response.data.msg}</strong>`
-     })
-     // .finally(function () {
-     //     // always executed
-     //
- });
+setUpTable()
 
+function populateActualData(table, courses) {
+  const grid = document.createElement('div')
+  grid.classList.add('grid')
+  for (const course of courses) {
+    const { courseId, courseName, facultyName, startDate, endDate } = course
+    const card = document.createElement('div')
+    card.classList.add('card')
+    const header = document.createElement('h2')
+    header.innerHTML = courseName
+    const faculty = document.createElement('p')
+    faculty.innerHTML = `Faculty: ${facultyName}`
+    const dates = document.createElement('p')
+    dates.innerHTML = `Schedule:${startDate} to ${endDate}`
+    const updateButton = document.createElement('button')
+    updateButton.innerHTML = 'Get Started'
+    updateButton.classList.add('btn', 'btn-success')
+    updateButton.setAttribute('data-id', courseId)
+    updateButton.addEventListener('click', (event) => {
+      const courseId = event.target.getAttribute('data-id')
+      window.location.href = `./UpdateCourse.html?id=${courseId}`
+    })
+    card.appendChild(header)
+    card.appendChild(faculty)
+    card.appendChild(dates)
+    card.appendChild(updateButton)
+    grid.appendChild(card)
+  }
+  table.appendChild(grid)
+}
+
+
+function apiFetchAllCourses(table) {
+  axios.get('http://localhost:8080/courses/list')
+      .then(res => {
+          const { data } = res
+          console.log(data)
+          const { sts, msg, bd } = data
+          populateActualData(table, bd)
+      })
+      .catch(err => console.log(err))
+}
