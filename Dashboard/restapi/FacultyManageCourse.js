5151f28a718384c4ed77489778715ee546cb8a65
ShwetaShirodkar <tasktask86@gmail.com>
2023-05-08 19:14:07 +0530

    Final commit

--- Dashboard/restapi/FacultyManageCourse.js
+++ Dashboard/restapi/FacultyManageCourse.js
@@ -1,11 +1,46 @@
 function setUpTable() {
     const table = document.getElementById('tableCourse')
+    const courseSearch = document.getElementById('courseSearch')
+  
+  
+    btnSearch.onclick = () => {
+  
+        const searchTerm = courseSearch.value.trim()
+  
+        if (searchTerm === '') {
+            alert('Please enter the course')
+            return
+        }
+  
+        apiFetchAllCourseByName(table, document.getElementById('courseSearch').value)
+  
+    }
+  
     apiFetchAllCourses(table)
-}
-
-setUpTable()
-
-function populateActualData(table, courses) {
+  
+  
+  }
+  
+  setUpTable()
+  
+  function populateActualData(table, courses) {
+  
+    // while (table.rows.length > 1) {
+    //   table.deleteRow(1);
+  
+    while (table.rows.length > 0) {
+      table.deleteRow(table.rows.length - 1);
+    }
+  
+    if (courses.length === 0) {
+      alert('No course found')
+      const row = table.insertRow()
+      const cell = row.insertCell(0)
+      cell.colSpan = 7
+      cell.innerHTML = 'No courses found.'
+      return
+  }
+  
     for (const course of courses) {
         const { courseId, courseName, facultyName, startDate, endDate, material, recording } = course
         const updatePageUrl = `./UpdateCourse.html?id=${courseId}`
@@ -21,25 +56,25 @@ function populateActualData(table, courses) {
             <a class="" href='${updatePageUrl}'>Update</a>
             <a class="" onclick='deleteCourse(${courseId})'>Delete</a>`
     }
-}
-
-function deleteCourse(courseId) {
+  }
+  
+  function deleteCourse(courseId) {
     console.log(courseId)
     //id = Number(id);
     axios.delete(`http://localhost:8080/courses/delete/${courseId}`)
         .then(function (response) {
             console.log('Course deleted')
             window.alert("Course deleted successfully")
-
+  
         })
         .catch(function (error) {
             // Handle error response
             console.log(error)
         })
-}
-
-function apiFetchAllCourses(table) {
-    axios.get('http://localhost:8080/courses/list')
+  }
+  
+  function apiFetchAllCourses(table) {
+    axios.get(`http://localhost:8080/courses/list`)
         .then(res => {
             const { data } = res
             console.log(data)
@@ -47,13 +82,27 @@ function apiFetchAllCourses(table) {
             populateActualData(table, bd)
         })
         .catch(err => console.log(err))
-}
-
-
-// function apiCallDeleteCourse(id, modal){
-//     const url= 'http://localhost:8080/course/${id}'
-//     axios.delete(url)
-//         .then(res => res.data)
-//         .then( ({ sts, msg, bd }) =>  modal.hide() )
-//         .catch(console.log)
-// }
+  }
+  
+  function apiFetchAllCourseByName(table, courseValue) {
+    const url = `http://localhost:8080/courses/name`
+    axios.get(url, {
+        params: {
+            courseName: courseValue
+        }
+    })
+        .then(res => {
+            const { data } = res
+            console.log(data)
+            const { sts, msg, bd } = data
+  
+            // if (bd.length === 0) alert("No course found")
+  
+            populateActualData(table, bd)
+  
+  
+        })
+        .catch(err => console.log(err))
+  }
+  
+  
